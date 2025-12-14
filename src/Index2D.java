9e73b5b66a315a1a34b8bf4e96b0912379b7d24a
ran-python <ran.rahamim@icloud.com>
2025-12-14 13:10:53 +0200

    Initial commit

---
+++ src/Index2D.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Test;
+
+public class Index2D implements Pixel2D {
+    public Index2D(int w, int h) {
+        ;
+    }
+    public Index2D(Pixel2D other) {
+        ;
+    }
+    @Override
+    public int getX() {
+
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+
+        return 0;
+    }
+
+    @Override
+    public double distance2D(Pixel2D p2) {
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        String ans = null;
+
+        return ans;
+    }
+
+    @Override
+    public boolean equals(Object p) {
+        boolean ans = true;
+
+        return ans;
+    }
+
+    static class Index2DTest {
+
+        @Test
+        void getX() {
+        }
+
+        @Test
+        void getY() {
+        }
+
+        @Test
+        void distance2D() {
+        }
+
+        @Test
+        void testToString() {
+        }
+
+        @Test
+        void testEquals() {
+        }
+    }
+}
---
+++ src/Index2DTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+class Index2DTest {
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void distance2D() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
