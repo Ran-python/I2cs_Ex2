8fb0961d0951ecb5cee77ce75ca96da5e843b001
ran-python <ran.rahamim@icloud.com>
2025-12-14 13:12:54 +0200

    test

--- src/MapTest.java
+++ src/MapTest.java
@@ -9,6 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class MapTest {
     /**
+     *
+     *
      */
     private int[][] _map_3_3 = {{0,1,0}, {1,0,1}, {0,1,0}};
     private Map2D _m0, _m1, _m3_3;
